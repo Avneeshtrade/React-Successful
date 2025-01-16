29f0f9ca6d62cbb5759d4c4504f40041caf28be2
Avneesh Dwivedi <avneesh.dwivedi@vlinkinfo.com>
2025-01-16 19:29:41 +0530

    addded build

---
+++ build/precache-manifest.d901777325eb52dbfa57177f9e11c549.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03fb10c6152c3c3b42da93dc9bacd7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef560774089c125f00c8",
+    "url": "/static/css/2.9798096d.chunk.css"
+  },
+  {
+    "revision": "6034d6520c2d4f860c2e",
+    "url": "/static/css/main.b513c3ac.chunk.css"
+  },
+  {
+    "revision": "ef560774089c125f00c8",
+    "url": "/static/js/2.3aa1436a.chunk.js"
+  },
+  {
+    "revision": "6034d6520c2d4f860c2e",
+    "url": "/static/js/main.9efa7a31.chunk.js"
+  },
+  {
+    "revision": "bc2f22dde9419903a20c",
+    "url": "/static/js/runtime~main.1be3cb92.js"
+  },
+  {
+    "revision": "b8c1b9ce297caf6193a63c3abb357240",
+    "url": "/static/media/bg.b8c1b9ce.jpg"
+  }
+]);
